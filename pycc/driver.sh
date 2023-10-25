234a922f37f8287f12b8bb58d9e5bbeb86a4dccc
romainducrocq <romain.g.ducrocq@gmail.com>
2023-10-25 14:36:54 +0200

    c1: add option skip linker

--- pycc/driver.sh
+++ pycc/driver.sh
@@ -28,8 +28,10 @@ if [ ${?} -ne 0 ]; then clean ${FILE} && exit 1; fi
 pack && python3 compiler.py ${FILE}.i ${@:1:$#-1}
 if [ ${?} -ne 0 ]; then clean ${FILE} && exit 1; fi
 
-# TODO
-# gcc ${FILE}.s -o ${FILE}
-# if [ ${?} -ne 0 ]; then clean ${FILE} && exit 1; fi
+echo ${@:1:$#-1} | grep -q -e "--lex" -e "--parse" -e "--codegen"
+if [ ${?} -ne 0 ]; then
+    gcc ${FILE}.s -o ${FILE}
+    if [ ${?} -ne 0 ]; then clean ${FILE} && exit 1; fi
+fi
 
 clean ${FILE} && exit 0
