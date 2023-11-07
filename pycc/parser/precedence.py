aad10543576996ddbb8b0199a422890d2c7fce7d
romainducrocq <romain.g.ducrocq@gmail.com>
2023-11-08 00:01:16 +0100

    c5: parser, add precedence

--- pycc/parser/precedence.py
+++ pycc/parser/precedence.py
@@ -31,7 +31,8 @@ TOKEN_PRECEDENCE: Dict[int, int] = {
     TOKEN_KIND.binop_bitxor: 20,
     TOKEN_KIND.binop_bitor: 15,
     TOKEN_KIND.binop_and: 10,
-    TOKEN_KIND.binop_or: 5
+    TOKEN_KIND.binop_or: 5,
+    TOKEN_KIND.operator_assignment: 1
 }
 
 
