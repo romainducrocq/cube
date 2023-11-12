4758aa67a868bc9ea6d6c26445f0621f3ce0119c
romainducrocq <romain.g.ducrocq@gmail.com>
2023-11-12 21:33:25 +0100

    refactor cythonize

---
+++ ccc/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.python/" ]; then
+    mkdir ~/.python/
+fi
+
+find ~/.python/ -maxdepth 1 -name "ccc" -type l -delete
+ln -s $(pwd) ~/.python/
+
+sudo find /usr/local/bin/ -maxdepth 1 -name "ccc" -type l -delete
+sudo ln -s $(pwd)/driver.sh /usr/local/bin/ccc
+
+exit 0
