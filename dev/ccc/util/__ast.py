12e781393fe5200c7a3a63d59d8f9868fa879780
romainducrocq <romain.g.ducrocq@gmail.com>
2023-11-13 19:42:58 +0100

    cythonize util.__ast

--- dev/ccc/util/__ast.py
+++ dev/ccc/util/__ast.py
@@ -1,92 +1,127 @@
-from typing import Any, Tuple, Generator
-
 __all__ = [
     'AST',
     'TIdentifier',
     'TInt',
     'ast_iter_child_nodes',
-    'ast_set_child_node'
+    'ast_set_child_node',
+    'ast_pretty_string'
 ]
 
 
-class AST:
+cdef class AST:
     """
     AST node
     """
-    _fields = ()
-
-    def pretty_string(self) -> str:
-        string = ""
-
-        def _pretty_string(kind: str = '<AST> ', node: Any = None, indent: int = 0) -> None:
-            nonlocal string
-            if not node:
-                node = self
-
-            string += str(' ' * indent + kind + type(node).__name__ + ':' + '\n')
-            indent += 4
-
-            def _pretty_string_child(_child_kind: str, _child_node: Any):
-                nonlocal string
-                if type(_child_node) in (str, int, type(None)):
-                    string += str(' ' * indent + _child_kind + type(_child_node).__name__ + ': '
-                                  + str(_child_node) + '\n')
-                else:
-                    _pretty_string(_child_kind, _child_node, indent)
-
-            for child_node, child_kind in ast_iter_fields(node):
-                if isinstance(child_node, list):
-                    string += str(' ' * indent + '<' + child_kind + '> List(' + str(len(child_node)) + '):' + '\n')
-                    indent += 4
-
-                    for e, list_node in enumerate(child_node):
-                        _pretty_string_child('[' + str(e) + '] ', list_node)
-
-                    indent -= 4
-
-                else:
-                    _pretty_string_child('<' + child_kind + '> ', child_node)
-
-        _pretty_string()
-        return string[:-1]
+    cdef public tuple[str] _fields
 
 
-class TIdentifier(AST):
+cdef class TIdentifier(AST):
     """ identifier str_t """
-    str_t: str = None
-    _fields = ('str_t',)
+    cdef public str str_t
 
-    def __init__(self, str_t: str):
+    def __cinit__(self):
+        self._fields = ('str_t',)
+
+    def __init__(self, str str_t):
         self.str_t = str_t
 
 
-class TInt(AST):
+cdef class TInt(AST):
     """ int int_t """
-    int_t: int = None
+    cdef public int int_t
 
-    _fields = ('int_t',)
+    def __cinit__(self):
+        self._fields = ('int_t',)
 
     def __init__(self, int_t: int):
         self.int_t = int_t
 
 
-def ast_iter_fields(node: AST) -> Generator[Tuple[Any, str], None, None]:
-    for field, name in [(getattr(node, _field), _field) for _field in node._fields]:
-        yield field, name
+cpdef list[tuple[object, str]] ast_iter_fields(AST node):
+    cdef str name
+    cdef list[tuple[object, str]] fields
+
+    fields = []
+    for name in node._fields:
+        fields.append((getattr(node, name), name))
+
+    return fields
 
 
-def ast_iter_child_nodes(node: AST) -> Generator[Tuple[Any, str, int], None, None]:
-    for field, name in ast_iter_fields(node):
+cpdef list[tuple[AST, str, int]] ast_iter_child_nodes(AST node):
+    cdef int e
+    cdef str name
+    cdef object field, item
+    cdef list[tuple[AST, str, int]] child_nodes
+
+    child_nodes = []
+    for name in node._fields:
+        field = getattr(node, name)
         if isinstance(field, AST):
-            yield field, name, -1
+            child_nodes.append((field, name, -1))
         elif isinstance(field, list):
             for e, item in enumerate(field):
                 if isinstance(item, AST):
-                    yield item, name, e
+                    child_nodes.append((item, name, e))
+
+    return child_nodes
 
 
-def ast_set_child_node(field: Any, name: str, index: int, set_node: Any) -> None:
+cpdef void ast_set_child_node(object field, str name, int index, AST set_node):
     if isinstance(field, AST):
         setattr(field, name, set_node)
     elif isinstance(getattr(field, name), list):
         getattr(field, name)[index] = set_node
+
+
+""" pretty string """
+
+cdef str string = ''
+cdef int indent = 0
+
+
+cpdef void _pretty_string_child(str _child_kind, object _child_node):
+    global string
+    global indent
+
+    if type(_child_node) in (str, int, type(None)):
+        string += str(' ' * indent + _child_kind + type(_child_node).__name__ + ': '
+                      + str(_child_node) + '\n')
+    else:
+        _pretty_string(_child_kind, _child_node)
+
+
+cpdef void _pretty_string(str kind, object node):
+    global string
+    global indent
+
+    cdef int e
+    cdef str child_kind
+    cdef object child_node
+    cdef list[object] list_node
+
+    string += str(' ' * indent + kind + type(node).__name__ + ':' + '\n')
+    indent += 4
+
+    for child_node, child_kind in ast_iter_fields(node):
+        if isinstance(child_node, list):
+            string += str(' ' * indent + '<' + child_kind + '> List(' + str(len(child_node)) + '):' + '\n')
+            indent += 4
+
+            for e, list_node in enumerate(child_node):
+                _pretty_string_child('[' + str(e) + '] ', list_node)
+
+            indent -= 4
+
+        else:
+            _pretty_string_child('<' + child_kind + '> ', child_node)
+
+
+cpdef str ast_pretty_string(AST node):
+    global string
+    global indent
+
+    string = ""
+    indent = 0
+    _pretty_string('<AST> ', node)
+    return string[:-1]
