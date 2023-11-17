37ed087a157f6ae767af565858ed6010fbd1fcee
romainducrocq <romain.g.ducrocq@gmail.com>
2023-11-17 16:40:11 +0100

    fix build

--- dev/build/build.sh
+++ dev/build/build.sh
@@ -11,16 +11,16 @@ function requirements () {
 function prebuild () {
     cd prebuild/
     ./build.sh
-    if [ ${?} -ne 0 ]; then exit 1; fi
+    if [ ${?} -ne 0 ]; then cd ../ && clean && exit 1; fi
     ./run.sh
-    if [ ${?} -ne 0 ]; then exit 1; fi
+    if [ ${?} -ne 0 ]; then cd ../ && clean && exit 1; fi
     cd ../
 }
 
 function cythonize () {
     cd ${PYX_TARGET}/
     cython -3 ${PYX_TARGET}.pyx
-    if [ ${?} -ne 0 ]; then exit 1; fi
+    if [ ${?} -ne 0 ]; then cd ../ && clean && exit 1; fi
     cd ../
 }
 
@@ -28,7 +28,9 @@ function compile () {
     if [ -f "${PYX_TARGET}/${PYX_TARGET}" ]; then rm ${PYX_TARGET}/${PYX_TARGET}; fi
     gcc -I${PYTHON_DIR} -I${PYTHON_DIR}Include -Wno-unused-result -Wsign-compare -DNDEBUG -g -fwrapv -O3 -Wall \
         -c ${PYX_TARGET}/*.c main.c
+    if [ ${?} -ne 0 ]; then clean && exit 1; fi
     gcc *.o -o ${PYX_TARGET}/${PYX_TARGET} -L${PYTHON_DIR} -lpython3.9 -lcrypt -lpthread -ldl  -lutil -lm -lm
+    if [ ${?} -ne 0 ]; then clean && exit 1; fi
 }
 
 function clean () {
