2985f30f191d99001dcfe3004a216d27db80b071
romainducrocq <romain.g.ducrocq@gmail.com>
2023-11-25 15:03:55 +0100

    cythonize assembly_codeemitter .3

--- lang/ccc/assembly_code_emitter.pyx
+++ lang/ccc/assembly_code_emitter.pyx
@@ -154,136 +154,170 @@ cdef str emit_unary_op(AsmUnaryOp node):
         "An error occurred in code emission, not all nodes were visited")
 
 
-# def emit_instruction(node: AST) -> None:
-#     """
-#     Mov(src, dst) ->
-#         $ movl <src>, <dst>
-#     Ret ->
-#         $ movq %rbp, %rsp
-#         $ popq %rbp
-#         $ ret
-#     Unary(unary_operator, operand) ->
-#         $ <unary_operator> <operand>
-#     Binary(binary_operator, src, dst) ->
-#         $ <binary_operator> <src>, <dst>
-#     Idiv(operand) ->
-#         $ idivl <operand>
-#     Cdq ->
-#         $ cdq
-#     AllocateStack(int) ->
-#         $ subq $<int>, %rsp
-#     Cmp(operand, operand) ->
-#         $ cmpl <operand>, <operand>
-#     Jmp(label) ->
-#         $ jmp .L<label>
-#     JmpCC(cond_code, label) ->
-#         $ j<cond_code> .L<label>
-#     SetCC(cond_code, operand) ->
-#         $ set<cond_code> <operand>
-#     Label(label) ->
-#         $ .L<label>:
-#     """
-#     expect_next(node, AsmInstruction)
-#     if isinstance(node, AsmMov):
-#         src: str = emit_operand(node.src, byte=4)
-#         dst: str = emit_operand(node.dst, byte=4)
-#         emit(f"movl {src}, {dst}", t=1)
-#     elif isinstance(node, AsmRet):
-#         emit("movq %rbp, %rsp", t=1)
-#         emit("popq %rbp", t=1)
-#         emit("ret", t=1)
-#     elif isinstance(node, AsmUnary):
-#         unary_op: str = emit_unary_op(node.unary_op)
-#         dst: str = emit_operand(node.dst, byte=4)
-#         emit(f"{unary_op} {dst}", t=1)
-#     elif isinstance(node, AsmBinary):
-#         binary_op: str = emit_binary_op(node.binary_op)
-#         src: str = emit_operand(node.src, byte=4)
-#         dst: str = emit_operand(node.dst, byte=4)
-#         emit(f"{binary_op} {src}, {dst}", t=1)
-#     elif isinstance(node, AsmIdiv):
-#         src: str = emit_operand(node.src, byte=4)
-#         emit(f"idivl {src}", t=1)
-#     elif isinstance(node, AsmCdq):
-#         emit("cdq", t=1)
-#     elif isinstance(node, AsmAllocStack):
-#         value: str = emit_int(node.value)
-#         emit(f"subq ${value}, %rsp", t=1)
-#     elif isinstance(node, AsmCmp):
-#         src: str = emit_operand(node.src, byte=4)
-#         dst: str = emit_operand(node.dst, byte=4)
-#         emit(f"cmpl {src}, {dst}", t=1)
-#     elif isinstance(node, AsmJmp):
-#         label: str = emit_identifier(node.target)
-#         emit(f"jmp .L{label}", t=1)
-#     elif isinstance(node, AsmJmpCC):
-#         cond_code: str = emit_condition_code(node.cond_code)
-#         label: str = emit_identifier(node.target)
-#         emit(f"j{cond_code} .L{label}", t=1)
-#     elif isinstance(node, AsmSetCC):
-#         cond_code: str = emit_condition_code(node.cond_code)
-#         dst: str = emit_operand(node.dst, byte=1)
-#         emit(f"set{cond_code} {dst}", t=1)
-#     elif isinstance(node, AsmLabel):
-#         label: str = emit_identifier(node.name)
-#         emit(f".L{label}:")
-#     else:
-#
-#         raise CodeEmitterError(
-#             "An error occurred in code emission, not all nodes were visited")
-#
-#
-# def emit_function_def(node: AST) -> None:
-#     """
-#     Function(name, instructions) ->
-#         $     .globl <name>
-#         $ <name>:
-#         $     pushq %rbp
-#         $     movq %rsp, %rbp
-#         $     <instructions>
-#     """
-#     expect_next(node, AsmFunctionDef)
-#     if isinstance(node, AsmFunction):
-#         name: str = emit_identifier(node.name)
-#         emit(f".globl {name}", t=1)
-#         emit(f"{name}:", t=0)
-#         emit("pushq %rbp", t=1)
-#         emit("movq %rsp, %rbp", t=1)
-#         for instruction in node.instructions:
-#             emit_instruction(instruction)
-#     else:
-#
-#         raise CodeEmitterError(
-#             "An error occurred in code emission, not all nodes were visited")
-#
-#
-# def emit_program(node: AST) -> None:
-#     """
-#     Program(function_definition) ->
-#         $ <function_definition>
-#         $     .section .note.GNU-stack,"",@progbits
-#     """
-#     expect_next(node, AST)
-#     if isinstance(node, AsmProgram):
-#         emit_function_def(node.function_def)
-#         emit(".section .note.GNU-stack,\"\",@progbits", t=1)
-#     else:
-#
-#         raise CodeEmitterError(
-#             "An error occurred in code emission, not all nodes were visited")
-#
-#
-# def code_emission(asm_ast: AST) -> List[str]:
-#     global asm_code
-#
-#     asm_code = []
-#     emit_program(asm_ast)
-#
-#     if not asm_code:
-#         raise CodeEmitterError(
-#             "An error occurred in code emission, ASM was not emitted")
-#
-#     return asm_code
+cdef void emit_ret_instructions(AsmRet node):
+    emit("movq %rbp, %rsp", t=1)
+    emit("popq %rbp", t=1)
+    emit("ret", t=1)
+
+
+cdef void emit_mov_instructions(AsmMov node):
+    cdef str src = emit_operand(node.src, byte=4)
+    cdef str dst = emit_operand(node.dst, byte=4)
+    emit(f"movl {src}, {dst}", t=1)
+
+
+cdef void emit_alloc_stack_instructions(AsmAllocStack node):
+    cdef str value = emit_int(node.value)
+    emit(f"subq ${value}, %rsp", t=1)
+
+
+cdef void emit_label_instructions(AsmLabel node):
+    cdef str label = emit_identifier(node.name)
+    emit(f".L{label}:")
+
+
+cdef void emit_cmp_instructions(AsmCmp node):
+    cdef str src = emit_operand(node.src, byte=4)
+    cdef str dst = emit_operand(node.dst, byte=4)
+    emit(f"cmpl {src}, {dst}", t=1)
+
+
+cdef void emit_jmp_instructions(AsmJmp node):
+    cdef str label = emit_identifier(node.target)
+    emit(f"jmp .L{label}", t=1)
+
+
+cdef void emit_jmp_cc_instructions(AsmJmpCC node):
+    cdef str cond_code = emit_condition_code(node.cond_code)
+    cdef str label = emit_identifier(node.target)
+    emit(f"j{cond_code} .L{label}", t=1)
+
+
+cdef void emit_set_cc_instructions(AsmSetCC node):
+    cdef str cond_code = emit_condition_code(node.cond_code)
+    cdef str dst = emit_operand(node.dst, byte=1)
+    emit(f"set{cond_code} {dst}", t=1)
+
+
+cdef void emit_unary_instructions(AsmUnary node):
+    cdef str unary_op = emit_unary_op(node.unary_op)
+    cdef str dst = emit_operand(node.dst, byte=4)
+    emit(f"{unary_op} {dst}", t=1)
+
+
+cdef void emit_binary_instructions(AsmBinary node):
+    cdef str binary_op = emit_binary_op(node.binary_op)
+    cdef str src = emit_operand(node.src, byte=4)
+    cdef str dst = emit_operand(node.dst, byte=4)
+    emit(f"{binary_op} {src}, {dst}", t=1)
+
+
+cdef void emit_idiv_instructions(AsmIdiv node):
+    cdef str src = emit_operand(node.src, byte=4)
+    emit(f"idivl {src}", t=1)
+
+
+cdef void emit_cdq_instructions(AsmCdq node):
+    emit("cdq", t=1)
+
+
+cdef void emit_instructions(AsmInstruction node):
+    # Ret                               -> $ movq %rbp, %rsp
+    #                                      $ popq %rbp
+    #                                      $ ret
+    # Mov(src, dst)                     -> $ movl <src>, <dst>
+    # AllocateStack(int)                -> $ subq $<int>, %rsp
+    # Label(label)                      -> $ .L<label>:
+    # Cmp(operand, operand)             -> $ cmpl <operand>, <operand>
+    # Jmp(label)                        -> $ jmp .L<label>
+    # JmpCC(cond_code, label)           -> $ j<cond_code> .L<label>
+    # SetCC(cond_code, operand)         -> $ set<cond_code> <operand>
+    # Unary(unary_operator, operand)    -> $ <unary_operator> <operand>
+    # Binary(binary_operator, src, dst) -> $ <binary_operator> <src>, <dst>
+    # Idiv(operand)                     -> $ idivl <operand>
+    # Cdq                               -> $ cdq
+    if isinstance(node, AsmRet):
+        emit_ret_instructions(node)
+        return
+    if isinstance(node, AsmMov):
+        emit_mov_instructions(node)
+        return
+    if isinstance(node, AsmAllocStack):
+        emit_alloc_stack_instructions(node)
+        return
+    if isinstance(node, AsmLabel):
+        emit_label_instructions(node)
+        return
+    if isinstance(node, AsmCmp):
+        emit_cmp_instructions(node)
+        return
+    if isinstance(node, AsmJmp):
+        emit_jmp_instructions(node)
+        return
+    if isinstance(node, AsmJmpCC):
+        emit_jmp_cc_instructions(node)
+        return
+    if isinstance(node, AsmSetCC):
+        emit_set_cc_instructions(node)
+        return
+    if isinstance(node, AsmUnary):
+        emit_unary_instructions(node)
+        return
+    if isinstance(node, AsmBinary):
+        emit_binary_instructions(node)
+        return
+    if isinstance(node, AsmIdiv):
+        emit_idiv_instructions(node)
+        return
+    if isinstance(node, AsmCdq):
+        emit_cdq_instructions(node)
+        return
+
+    raise RuntimeError(
+        "An error occurred in code emission, not all nodes were visited")
+
+
+cdef void emit_function_def(AsmFunctionDef node):
+    # Function(name, instructions) -> $     .globl <name>
+    #                                 $ <name>:
+    #                                 $     pushq %rbp
+    #                                 $     movq %rsp, %rbp
+    #                                 $     <instructions>
+    cdef str name
+    if isinstance(node, AsmFunction):
+        name = emit_identifier(node.name)
+        emit(f".globl {name}", t=1)
+        emit(f"{name}:", t=0)
+        emit("pushq %rbp", t=1)
+        emit("movq %rsp, %rbp", t=1)
+        for instruction in node.instructions:
+            emit_instructions(instruction)
+        return
+
+    raise RuntimeError(
+        "An error occurred in code emission, not all nodes were visited")
+
+
+cdef void emit_program(AST node):
+    # Program(function_definition) -> $ <function_definition>
+    #                                 $     .section .note.GNU-stack,"",@progbits
+    if isinstance(node, AsmProgram):
+        emit_function_def(node.function_def)
+        emit(".section .note.GNU-stack,\"\",@progbits", t=1)
+        return
+
+    raise RuntimeError(
+        "An error occurred in code emission, not all nodes were visited")
+
 
 cdef list[str] code_emission(AST asm_ast):
-    return []
+    global asm_code
+
+    asm_code = []
+    emit_program(asm_ast)
+
+    if not asm_code:
+        raise RuntimeError(
+            "An error occurred in code emission, ASM was not emitted")
+
+    return asm_code
