098f1d8cdf6aa609dbb40b1c6ffcdf4f02834a0c
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-06 20:06:30 +0100

    [8] validate fix param check

--- lang/dev/test-suite.sh
+++ lang/dev/test-suite.sh
@@ -17,14 +17,9 @@ ln -s ../LANG-CCC/${PACKAGE_NAME}/* .
 if [ ${#} -ne 0 ]; then
     test ${@}
 else
-    for i in $(seq 1 8); do
-#        test --chapter ${i} --latest-only --bitwise --compound --goto
-        if [[ ${i} -eq 5 ]]; then
-            continue
-        fi
+    for i in $(seq 1 9); do
         test --chapter ${i} --stage validate --latest-only --bitwise --compound --goto
     done
-#    test --chapter 9 --stage parse --latest-only
 fi
 
 exit 0
