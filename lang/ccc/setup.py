83e9315ee9e4e208a0a57343a24f5be0c29f4ae4
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-10 15:41:34 +0100

    [9] prapare codegen

--- lang/ccc/setup.py
+++ lang/ccc/setup.py
@@ -28,10 +28,10 @@ ext_modules = [
     Extension(f"{PACKAGE_NAME}.intermediate_tac_ast", ["./intermediate_tac_ast.pyx"]),
     Extension(f"{PACKAGE_NAME}.intermediate_three_address_generator", ["./intermediate_three_address_generator.pyx"]),
     Extension(f"{PACKAGE_NAME}.assembly_asm_ast", ["./assembly_asm_ast.pyx"]),
-    # Extension(f"{PACKAGE_NAME}.assembly_assembly_generator", ["./assembly_assembly_generator.pyx"]),
+    Extension(f"{PACKAGE_NAME}.assembly_assembly_generator", ["./assembly_assembly_generator.pyx"]),
     # Extension(f"{PACKAGE_NAME}.assembly_code_emitter", ["./assembly_code_emitter.pyx"]),
-    # Extension(f"{PACKAGE_NAME}.assembly_register", ["./assembly_register.pyx"]),
-    # Extension(f"{PACKAGE_NAME}.assembly_stack_corrector", ["./assembly_stack_corrector.pyx"]),
+    Extension(f"{PACKAGE_NAME}.assembly_register", ["./assembly_register.pyx"]),
+    Extension(f"{PACKAGE_NAME}.assembly_stack_corrector", ["./assembly_stack_corrector.pyx"]),
 ]
 
 for ext_module in ext_modules:
