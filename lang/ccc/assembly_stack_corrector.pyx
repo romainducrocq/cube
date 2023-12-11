e97c9b6740b07cc4d199b9f1edf8540ed07894c2
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-11 13:14:44 +0100

    [9] refactor pseudo registers

--- lang/ccc/assembly_stack_corrector.pyx
+++ lang/ccc/assembly_stack_corrector.pyx
@@ -1,7 +1,7 @@
-from ccc.util_ast cimport ast_iter_child_nodes, ast_set_child_node
-from ccc.assembly_asm_ast cimport AST, TInt, AsmProgram, AsmFunctionDef, AsmFunction, AsmPseudo, AsmStack, AsmAllocStack
-from ccc.assembly_asm_ast cimport AsmInstruction, AsmOperand, AsmMov, AsmCmp, AsmImm, AsmBinary, AsmIdiv, AsmMult
-from ccc.assembly_asm_ast cimport AsmAdd, AsmSub, AsmBitAnd, AsmBitOr, AsmBitXor, AsmBitShiftLeft, AsmBitShiftRight
+from ccc.assembly_asm_ast cimport TInt, AsmProgram, AsmFunctionDef, AsmFunction, AsmPseudo, AsmStack, AsmAllocStack
+from ccc.assembly_asm_ast cimport AsmInstruction, AsmOperand, AsmImm, AsmMov, AsmPush, AsmCmp, AsmSetCC
+from ccc.assembly_asm_ast cimport AsmUnary, AsmBinary, AsmAdd, AsmSub, AsmIdiv, AsmMult
+from ccc.assembly_asm_ast cimport AsmBitAnd, AsmBitOr, AsmBitXor, AsmBitShiftLeft, AsmBitShiftRight
 from ccc.assembly_register cimport REGISTER_KIND, generate_register
 
 
@@ -10,36 +10,81 @@ cdef int counter = -1
 cdef dict[str, int] pseudo_map = {}
 
 
-cdef void replace_pseudo_registers(AST node):
+cdef AsmStack replace_pseudo_register_stack(AsmPseudo node):
     global counter
     global pseudo_map
 
-    if counter == -1:
-        counter = 0
+    if node.name.str_t not in pseudo_map:
+        counter += OFFSET
+        pseudo_map[node.name.str_t] = counter
 
-    cdef int item
-    cdef str attr
-    cdef TInt value
-    cdef AST child_node
-    for child_node, attr, item in ast_iter_child_nodes(node):
-        if isinstance(child_node, AsmPseudo):
-            if child_node.name.str_t not in pseudo_map:
-                counter += OFFSET
-                pseudo_map[child_node.name.str_t] = counter
+    cdef value = TInt(pseudo_map[node.name.str_t])
+    return AsmStack(value)
 
-            value = TInt(pseudo_map[child_node.name.str_t])
-            ast_set_child_node(node, attr, item, AsmStack(value))
 
-        else:
-            replace_pseudo_registers(child_node)
+cdef AsmStack replace_operand_pseudo_register(AsmOperand node):
+    return replace_pseudo_register_stack(node)
+
+
+cdef void replace_mov_pseudo_registers(AsmMov node):
+    if isinstance(node.src, AsmPseudo):
+        node.src = replace_operand_pseudo_register(node.src)
+    if isinstance(node.dst, AsmPseudo):
+        node.dst = replace_operand_pseudo_register(node.dst)
+
+
+cdef void replace_push_pseudo_registers(AsmPush node):
+    if isinstance(node.src, AsmPseudo):
+        node.src = replace_operand_pseudo_register(node.src)
+
+
+cdef void replace_cmp_pseudo_registers(AsmCmp node):
+    if isinstance(node.src, AsmPseudo):
+        node.src = replace_operand_pseudo_register(node.src)
+    if isinstance(node.dst, AsmPseudo):
+        node.dst = replace_operand_pseudo_register(node.dst)
+
+
+cdef void replace_set_cc_pseudo_registers(AsmSetCC node):
+    if isinstance(node.dst, AsmPseudo):
+        node.dst = replace_operand_pseudo_register(node.dst)
+
+
+cdef void replace_unary_pseudo_registers(AsmUnary node):
+    if isinstance(node.dst, AsmPseudo):
+        node.dst = replace_operand_pseudo_register(node.dst)
+
+
+cdef void replace_binary_pseudo_registers(AsmBinary node):
+    if isinstance(node.src, AsmPseudo):
+        node.src = replace_operand_pseudo_register(node.src)
+    if isinstance(node.dst, AsmPseudo):
+        node.dst = replace_operand_pseudo_register(node.dst)
+
+
+cdef void replace_idiv_pseudo_registers(AsmIdiv node):
+    if isinstance(node.src, AsmPseudo):
+        node.src = replace_operand_pseudo_register(node.src)
+
+
+cdef void replace_pseudo_registers(AsmInstruction node):
+    if isinstance(node, AsmMov):
+        replace_mov_pseudo_registers(node)
+    elif isinstance(node, AsmPush):
+        replace_push_pseudo_registers(node)
+    elif isinstance(node, AsmCmp):
+        replace_cmp_pseudo_registers(node)
+    elif isinstance(node, AsmSetCC):
+        replace_set_cc_pseudo_registers(node)
+    elif isinstance(node, AsmUnary):
+        replace_unary_pseudo_registers(node)
+    elif isinstance(node, AsmBinary):
+        replace_binary_pseudo_registers(node)
+    elif isinstance(node, AsmIdiv):
+        replace_idiv_pseudo_registers(node)
 
 
 cdef void prepend_alloc_stack(list[AsmInstruction] instructions):
-
-    if counter == -1:
-        raise RuntimeError(
-            "An error occurred in stack management, stack was not allocated")
-
     cdef TInt value = TInt(-1 * counter)
     instructions.insert(0, AsmAllocStack(value))
 
@@ -125,10 +170,12 @@ cdef void correct_instructions(AsmProgram node):
     global pseudo_map
 
     cdef int function_def
+    cdef int instruction
     for function_def in range(len(node.function_defs)):
-        counter = -1
+        counter = 0
         pseudo_map = {}
-        replace_pseudo_registers(node.function_defs[function_def])
+        for instruction in  range(len(node.function_defs[function_def].instructions)):
+            replace_pseudo_registers(node.function_defs[function_def].instructions[instruction])
         correct_function_def(node.function_defs[function_def])
 
 
