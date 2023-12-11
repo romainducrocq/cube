c238b79cc53c60e6935f2c9922e508d21c4c35e6
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-11 13:50:33 +0100

    [9] preprocess iter ast

--- lang/ccc/util_ast.pyx
+++ lang/ccc/util_ast.pyx
@@ -30,32 +30,32 @@ cdef list[tuple[object, str]] ast_iter_fields(AST node): #
 #
     return fields #
 #
-
-cdef list[tuple[AST, str, int]] ast_iter_child_nodes(AST node):
-
-    cdef object field
-    cdef list[tuple[AST, str, int]] child_nodes = []
-
-    cdef int name
-    cdef int item
-    for name in range(len(node._fields)):
-        field = getattr(node, node._fields[name])
-        if isinstance(field, AST):
-            child_nodes.append((field, node._fields[name], -1))
-        elif isinstance(field, list):
-            for item in range(len(field)):
-                if isinstance(field[item], AST):
-                    child_nodes.append((field[item], node._fields[name], item))
-
-    return child_nodes
-
-
-cdef void ast_set_child_node(object field, str name, int index, AST set_node):
-    if isinstance(field, AST):
-        setattr(field, name, set_node)
-    elif isinstance(getattr(field, name), list):
-        getattr(field, name)[index] = set_node
-
+#
+cdef list[tuple[AST, str, int]] ast_iter_child_nodes(AST node): #
+#
+    cdef object field #
+    cdef list[tuple[AST, str, int]] child_nodes = [] #
+#
+    cdef int name #
+    cdef int item #
+    for name in range(len(node._fields)): #
+        field = getattr(node, node._fields[name]) #
+        if isinstance(field, AST): #
+            child_nodes.append((field, node._fields[name], -1)) #
+        elif isinstance(field, list): #
+            for item in range(len(field)): #
+                if isinstance(field[item], AST): #
+                    child_nodes.append((field[item], node._fields[name], item)) #
+#
+    return child_nodes #
+#
+#
+cdef void ast_set_child_node(object field, str name, int index, AST set_node): #
+    if isinstance(field, AST): #
+        setattr(field, name, set_node) #
+    elif isinstance(getattr(field, name), list): #
+        getattr(field, name)[index] = set_node #
+#
 #
 # pretty string
 #
