bd4a00724226eb7293aac7309ad31289cf8554e2
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-13 17:06:39 +0100

    [10] update symbol table

--- lang/ccc/semantic_symbol_table.pxd
+++ lang/ccc/semantic_symbol_table.pxd
@@ -8,3 +8,37 @@ cdef class Int(Type):
 
 cdef class FunType(Type):
     cdef public int param_count
+
+
+cdef class InitialValue:
+    pass
+
+
+cdef class Tentative(InitialValue):
+    pass
+
+
+cdef class Initial(InitialValue):
+    cdef public int value
+
+
+cdef class NoInitializer(InitialValue):
+    pass
+
+
+cdef class IdentifierAttr:
+    pass
+
+
+cdef class FunAttr(IdentifierAttr):
+    cdef public bint is_defined
+    cdef public bint is_global
+
+
+cdef class StaticAttr(IdentifierAttr):
+    cdef public InitialValue init
+    cdef public bint is_global
+
+
+cdef class LocalAttr(IdentifierAttr):
+    pass
--- lang/ccc/semantic_symbol_table.pyx
+++ lang/ccc/semantic_symbol_table.pyx
@@ -1,5 +1,5 @@
 cdef class Type:
-    # Type = Int | FunType(int)
+    # type = Int | FunType(int)
     pass
 
 
@@ -12,3 +12,52 @@ cdef class FunType(Type):
     # FunType(int param_count)
     def __init__(self, int param_count):
         self.param_count = param_count
+
+
+cdef class InitialValue:
+    # initial_value = Tentative
+    #               | Initial(int)
+    #               | NoInitializer
+    pass
+
+
+cdef class Tentative(InitialValue):
+    # Tentative
+    pass
+
+
+cdef class Initial(InitialValue):
+    # Initial(int)
+    def __init__(self, int value):
+        self.value = value
+
+
+cdef class NoInitializer(InitialValue):
+    # NoInitializer
+    pass
+
+
+cdef class IdentifierAttr:
+    # identifier_attrs = FunAttr(bool defined, bool global)
+    #                  | StaticAttr(initial_value init, bool global)
+    #                  | LocalAttr
+    pass
+
+
+cdef class FunAttr(IdentifierAttr):
+    # FunAttr(bool defined, bool global)
+    def __init__(self, bint is_defined, bint is_global):
+        self.is_defined = is_defined
+        self.is_global = is_global
+
+
+cdef class StaticAttr(IdentifierAttr):
+    # StaticAttr(initial_value init, bool global)
+    def __init__(self, InitialValue init, bint is_global):
+        self.init = init
+        self.is_global = is_global
+
+
+cdef class LocalAttr(IdentifierAttr):
+    # LocalAttr
+    pass
