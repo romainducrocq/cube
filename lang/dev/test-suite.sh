b876cb3cb26625a8d0000abfff15876ab6f85349
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-13 10:41:07 +0100

    [10] prepare parse

--- lang/dev/test-suite.sh
+++ lang/dev/test-suite.sh
@@ -17,10 +17,9 @@ ln -s ../LANG-CCC/${PACKAGE_NAME}/* .
 if [ ${#} -ne 0 ]; then
     test ${@}
 else
-    for i in $(seq 1 9); do
-        test --chapter ${i} --latest-only --bitwise --compound --goto
+    for i in $(seq 1 10); do
+        test --chapter ${i} --stage lex --latest-only --bitwise --compound --goto
     done
-    test --chapter 10 --stage lex --latest-only --bitwise --compound --goto
 fi
 
 exit 0
