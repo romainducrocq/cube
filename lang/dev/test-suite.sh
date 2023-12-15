8efa7fe6b0ff854d7bcc50e3e4d7844ff4521e57
romainducrocq <romain.g.ducrocq@gmail.com>
2023-12-15 11:59:59 +0100

    [10] fix global tac top_levels

--- lang/dev/test-suite.sh
+++ lang/dev/test-suite.sh
@@ -17,7 +17,7 @@ ln -s ../LANG-CCC/${PACKAGE_NAME}/* .
 if [ ${#} -ne 0 ]; then
     test ${@}
 else
-    for i in $(seq 1 9); do
+    for i in $(seq 1 10); do
         test --chapter ${i} --latest-only --bitwise --compound --goto
     done
 fi
